c34611d0458da71b9d732089a4159d36531fab9d
Alex Andrutskii <alex.andrutskii@gmail.com>
2019-09-24 11:01:09 +0300

    Updates

---
+++ precache-manifest.1f327875e1e40125af7d8425f658d042.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a4bdade66a1c1146d882e239c032330",
+    "url": "/react_dynamic-list-of-posts/index.html"
+  },
+  {
+    "revision": "bf557168ab570a97ae8e",
+    "url": "/react_dynamic-list-of-posts/static/css/main.4c6d5666.chunk.css"
+  },
+  {
+    "revision": "6b65a40088deff272b22",
+    "url": "/react_dynamic-list-of-posts/static/js/2.b0c2f2c9.chunk.js"
+  },
+  {
+    "revision": "bf557168ab570a97ae8e",
+    "url": "/react_dynamic-list-of-posts/static/js/main.90a0a66a.chunk.js"
+  },
+  {
+    "revision": "fc988510490290486853",
+    "url": "/react_dynamic-list-of-posts/static/js/runtime~main.69709ad2.js"
+  }
+]);
